147477b8a5f2cb3352cb6e29bff90facf49f592f
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2025-06-24 12:00:26 +0530

    Method level docstiring written for utililty classes & Functions

--- ETL-Airflow/dags/tasks/utils.py
+++ ETL-Airflow/dags/tasks/utils.py
@@ -7,16 +7,39 @@ from tasks.my_secrets import PASSWORD, USERNAME, SERVICE_KEY
 # Set the Level of logging to be INFO by default
 logging.basicConfig(level=logging.INFO)
 
+
 # Create a Class : APIClient to be used when ever API needs to be called
 class APIClient:
+    """
+    A Client Class to interact with APIs.
 
-    # Define the constructor to use the default URL
+    Attributes:
+    base_url (String): The base URL of the API Requests.
+    """
     def __init__(self, base_url="http://host.docker.internal:8000/v1"):
+        """
+        Initializes the API Client with default or Provided base URL.
+
+        Parameters : 
+        base_url (String): The base url of the API
+        """
         self.base_url = base_url
 
     # Define a method to fetch the data when the API is called
     def fetch_data(self, api_type: str, auth=False):
+        """
+        Fetches data from the API Endpoint.
 
+        Parameters:
+        api_type (String): The API endpoint to call.
+        auth (Boolean): Whether the authentication is needed.
+
+        Returns:
+        response (Dictionary): The JSON response from the API.
+
+        Raises: 
+        Exception: If the API Call fails.
+        """
         try :
             import requests
 
@@ -47,18 +70,29 @@ class APIClient:
             logging.error("Exception raised..!")
             raise e
 
+
 # Custom Exception raised when duplicates are found in the dataset
 class DuplicateException(Exception):
-
+    """Custom Exception Raised when Duplicates are found in the Dataset"""
     def __init__(self, message='Duplicates are found in Dataset.'):
         super().__init__(message)
 
+
 # Create a class named DuplicateChecker
 class DuplicateChecker:
-
-    # Create a class method for this class
+    """This is the Class which contains the methods required for checking the Duplicates"""
     @classmethod
     def has_duplicates(cls, df, primary_key_list : list):
+        """
+        Checks for the Duplicates in the Dataframe based on the List of Primary key Columns provided.
+
+        Parameters:
+        df (Dataframe): The Spark Dataframe to check.
+        primary_key_list (list): List of column names to be used as the Primary Keys.
+
+        Raises:
+        Duplicate Exception if the Duplicates are found.
+        """
         logging.info("Checking for the duplicates in the provided Dataset")
         grouped_df = df.groupBy(primary_key_list) \
                             .agg(count('*').alias('cnt'))\
@@ -70,7 +104,9 @@ class DuplicateChecker:
 
 # Define a function to get a spark session
 def get_spark_session() :
-
+    """
+    This Function returns the spark session
+    """
     logging.info("Creating a Spark Session")
 
     # Create a spark session
@@ -83,9 +119,22 @@ def get_spark_session() :
     logging.info("Spark session Created")
     return spark
 
-# Define a function to read the data from the Postgres Database
-def read_data(spark, table) :
 
+# Define a function named read_data
+def read_data(spark, table) :
+    """
+    Reads the data from the Postgres Database
+
+    Parameters:
+    spark (Spark Session): The Spark session with the required Jars
+    table (String): Reading the Table using JDBC from the PG Admin
+
+    Returns: 
+    The Dataframe built on by reading data from the table.
+
+    Raises:
+    Exception: In case of Any Failure while reading from the table.
+    """
     try :
         logging.info("Connecting to PostgreSQL database using JDBC driver...")
         df = spark.read.format("jdbc")\
@@ -101,9 +150,21 @@ def read_data(spark, table) :
         raise e
     return df
 
-# This function is defined to Write the data into the PG Database
-def write_into_table(table, data_frame, schema, strategy):
 
+# Define a function named write_into_table
+def write_into_table(table, data_frame, schema, strategy):
+    """
+    Writes the data into the Postgres Table
+
+    Parameters:
+    data_frame (Dataframe): The Spark Dataframe
+    table (String): The Table where we want to write the data_frame into Table
+    schema (String): The Schema where we want to store the table
+    strategy (String): The type of the insertion into table (append/overwrite)
+
+    Returns: 
+    The Dataframe built after writing data into the table
+    """
     try : 
         logging.info("Connecting to PostgreSQL database using JDBC driver...")
         logging.info(f"Established connection. Writing into {schema}.{table}")
@@ -121,12 +182,25 @@ def write_into_table(table, data_frame, schema, strategy):
         raise e
     return df
 
+
+# Define a function named write_to_gcs
 def write_to_gcs(dataframe, location):
-        logging.info("Authenticating to GCS to load the data into parquet file..")
-        dataframe.write.mode("append").parquet(f"gs://reporting-lgcy/{location}")
-        logging.info(f"Loaded into Parquet File : {location}")
+    """
+    Writes the data into GCS Bucket Location
+
+    Parameters:
+    dataframe (Dataframe): The Spark Dataframe which we want to write
+    location (String): The target GCS Location where to write
+    """
+    logging.info("Authenticating to GCS to load the data into parquet file..")
+    dataframe.write.mode("append").parquet(f"gs://reporting-lgcy/{location}")
+    logging.info(f"Loaded into Parquet File : {location}")
+
 
 # Define Function to abort the Spark Session used up to the instance
 def abort_session(spark):
+    """
+    Stops the Spark Session taking itself as a parameter.
+    """
     spark.stop()
     logging.info("Spark Session ended.!")
