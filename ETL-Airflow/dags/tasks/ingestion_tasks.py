68d2851c137651b8698518f675b6c62e6629ca27
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2025-07-09 00:12:17 +0530

    Code Standards - Ingestion task

--- ETL-Airflow/dags/tasks/ingestion_tasks.py
+++ ETL-Airflow/dags/tasks/ingestion_tasks.py
@@ -5,7 +5,9 @@ from pyspark.sql.functions import current_date, col
 import logging
 from datetime import datetime
 import pytz
-from tasks.utils import get_spark_session, write_into_table, abort_session, APIClient, DuplicateChecker, DuplicateException, write_to_gcs
+from tasks.utils import get_spark_session, write_into_table, abort_session, \
+    APIClient, DuplicateChecker, DuplicateException, write_to_gcs
+
 
 # Create a task that helps in ingesting the data into Suppliers
 @task(task_id="m_ingest_data_into_suppliers")
@@ -23,7 +25,7 @@ def supplier_data_ingestion():
 
     # Get a spark session
     spark = get_spark_session()
-    
+
     # Set the Suppliers value to fetch the data
     api = "suppliers"
     response = client.fetch_data(api)
@@ -34,25 +36,25 @@ def supplier_data_ingestion():
 
     # Do the Transformations for the Suppliers Dataframe
     suppliers_df = suppliers_df \
-                    .withColumnRenamed(suppliers_df.columns[0], "SUPPLIER_ID") \
-                    .withColumnRenamed(suppliers_df.columns[1], "SUPPLIER_NAME") \
-                    .withColumnRenamed(suppliers_df.columns[2], "CONTACT_DETAILS") \
-                    .withColumnRenamed(suppliers_df.columns[3], "REGION")
+        .withColumnRenamed(suppliers_df.columns[0], "SUPPLIER_ID") \
+        .withColumnRenamed(suppliers_df.columns[1], "SUPPLIER_NAME") \
+        .withColumnRenamed(suppliers_df.columns[2], "CONTACT_DETAILS") \
+        .withColumnRenamed(suppliers_df.columns[3], "REGION")
     logging.info("Data Frame : Transformed 'suppliers_df' is built")
 
     # Populating the legacy version of Suppliers Data
     suppliers_df_lgcy = suppliers_df \
-                            .withColumn("DAY_DT", current_date()) \
-                            .select(
-                                col("DAY_DT"),
-                                col("SUPPLIER_ID"),
-                                col("SUPPLIER_NAME"),
-                                col("CONTACT_DETAILS"),
-                                col("REGION")
-                            )
+        .withColumn("DAY_DT", current_date()) \
+        .select(
+            col("DAY_DT"),
+            col("SUPPLIER_ID"),
+            col("SUPPLIER_NAME"),
+            col("CONTACT_DETAILS"),
+            col("REGION")
+        )
     logging.info("Data Frame : 'suppliers_df_lgcy' is built")
 
-    try :
+    try:
 
         # Implement the Duplicate checker
         chk = DuplicateChecker()
@@ -71,12 +73,13 @@ def supplier_data_ingestion():
         logging.error(str(e))
         raise
 
-    finally :
+    finally:
 
         # Abort the session when Done.
         abort_session(spark)
 
     return f"{api} data ingested successfully!"
+
 
 # Create a task that helps in ingesting the data into Customers
 @task(task_id="m_ingest_data_into_customers")
@@ -93,7 +96,7 @@ def customer_data_ingestion():
 
     # Get a spark session
     spark = get_spark_session()
-    
+
     # Set the Customers value to fetch the data
     api = "customers"
     response = client.fetch_data(api, True)
@@ -101,30 +104,30 @@ def customer_data_ingestion():
     # Create a data frame from the response of the API
     customer_df = spark.createDataFrame(Row(**x) for x in response['data'])
     logging.info("Data Frame : 'customer_df' is built")
-    
+
     # Do the transformations for the customers Dataframe
     customer_df = customer_df \
-                    .withColumnRenamed(customer_df.columns[0], "CUSTOMER_ID") \
-                    .withColumnRenamed(customer_df.columns[1], "NAME") \
-                    .withColumnRenamed(customer_df.columns[2], "CITY") \
-                    .withColumnRenamed(customer_df.columns[3], "EMAIL") \
-                    .withColumnRenamed(customer_df.columns[4], "PHONE_NUMBER")
+        .withColumnRenamed(customer_df.columns[0], "CUSTOMER_ID") \
+        .withColumnRenamed(customer_df.columns[1], "NAME") \
+        .withColumnRenamed(customer_df.columns[2], "CITY") \
+        .withColumnRenamed(customer_df.columns[3], "EMAIL") \
+        .withColumnRenamed(customer_df.columns[4], "PHONE_NUMBER")
     logging.info("Data Frame : Transformed 'customer_df' is built")
 
     # Populating the legacy version of Customers Data
     customer_df_lgcy = customer_df \
-                            .withColumn("DAY_DT", current_date()) \
-                            .select(
-                                col("DAY_DT"),
-                                col("CUSTOMER_ID"),
-                                col("NAME"),
-                                col("CITY"),
-                                col("EMAIL"),
-                                col("PHONE_NUMBER")
-                            )
+        .withColumn("DAY_DT", current_date()) \
+        .select(
+            col("DAY_DT"),
+            col("CUSTOMER_ID"),
+            col("NAME"),
+            col("CITY"),
+            col("EMAIL"),
+            col("PHONE_NUMBER")
+        )
     logging.info("Data Frame : 'customer_df_lgcy' is built")
 
-    try :
+    try:
 
         # Implement the Duplicate checker
         chk = DuplicateChecker()
@@ -143,12 +146,13 @@ def customer_data_ingestion():
         logging.error(str(e))
         raise
 
-    finally :
+    finally:
 
         # Abort the session when Done.
         abort_session(spark)
 
     return f"{api} data ingested successfully!"
+
 
 # Create a task that helps in ingesting the data into Products
 @task(task_id="m_ingest_data_into_products")
@@ -166,44 +170,44 @@ def products_data_ingestion():
 
     # Get a spark session
     spark = get_spark_session()
-    
+
     # Set the Products value to fetch the data
     api = "products"
     response = client.fetch_data(api)
-    
+
     # Create a data frame from the response of the API
     product_df = spark.createDataFrame(Row(**x) for x in response['data'])
     logging.info("Data Frame : 'product_df' is built")
 
     # Do the Transformation for the product Dataframe
     product_df = product_df \
-                    .withColumnRenamed(product_df.columns[0], "PRODUCT_ID") \
-                    .withColumnRenamed(product_df.columns[1], "PRODUCT_NAME") \
-                    .withColumnRenamed(product_df.columns[2], "CATEGORY") \
-                    .withColumnRenamed(product_df.columns[3], "SELLING_PRICE") \
-                    .withColumnRenamed(product_df.columns[4], "COST_PRICE") \
-                    .withColumnRenamed(product_df.columns[5], "STOCK_QUANTITY") \
-                    .withColumnRenamed(product_df.columns[6], "REORDER_LEVEL") \
-                    .withColumnRenamed(product_df.columns[7], "SUPPLIER_ID")
+        .withColumnRenamed(product_df.columns[0], "PRODUCT_ID") \
+        .withColumnRenamed(product_df.columns[1], "PRODUCT_NAME") \
+        .withColumnRenamed(product_df.columns[2], "CATEGORY") \
+        .withColumnRenamed(product_df.columns[3], "SELLING_PRICE") \
+        .withColumnRenamed(product_df.columns[4], "COST_PRICE") \
+        .withColumnRenamed(product_df.columns[5], "STOCK_QUANTITY") \
+        .withColumnRenamed(product_df.columns[6], "REORDER_LEVEL") \
+        .withColumnRenamed(product_df.columns[7], "SUPPLIER_ID")
     logging.info("Data Frame : Transformed 'product_df' is built")
 
     # Populating the legacy version of Products Data
     product_df_lgcy = product_df \
-                            .withColumn("DAY_DT", current_date()) \
-                            .select(
-                                col("DAY_DT"),
-                                col("PRODUCT_ID"),
-                                col("PRODUCT_NAME"),
-                                col("CATEGORY"),
-                                col("SELLING_PRICE"),
-                                col("COST_PRICE"),
-                                col("STOCK_QUANTITY"),
-                                col("REORDER_LEVEL"),
-                                col("SUPPLIER_ID")
-                            )
+        .withColumn("DAY_DT", current_date()) \
+        .select(
+            col("DAY_DT"),
+            col("PRODUCT_ID"),
+            col("PRODUCT_NAME"),
+            col("CATEGORY"),
+            col("SELLING_PRICE"),
+            col("COST_PRICE"),
+            col("STOCK_QUANTITY"),
+            col("REORDER_LEVEL"),
+            col("SUPPLIER_ID")
+        )
     logging.info("Data Frame : 'product_df_lgcy' is built")
 
-    try :
+    try:
 
         # Implement the Duplicate checker
         chk = DuplicateChecker()
@@ -222,12 +226,13 @@ def products_data_ingestion():
         logging.error(str(e))
         raise
 
-    finally :
+    finally:
 
         # Abort the session when Done.
         abort_session(spark)
 
     return f"{api} data ingested successfully!"
+
 
 # Create a task that helps the data in ingesting the data into sales
 @task(task_id="m_ingest_data_into_sales")
@@ -243,47 +248,51 @@ def sales_data_ingestion():
     india_tz = pytz.timezone("Asia/Kolkata")
     today = datetime.now(india_tz).strftime("%Y%m%d")
 
-    # Create a spark session with the hadoop configurations and also authentic credentials
+    # Get the spark session
     spark = get_spark_session()
 
     # Create a data frame by reading the CSV from the Google Bucket
-    sales_df = spark.read.csv(f'gs://meta-morph-flow/{today}/sales_{today}.csv', header=True, inferSchema=True)
+    sales_df = spark.read.csv(
+                            f'gs://meta-morph-flow/{today}/sales_{today}.csv',
+                            header=True,
+                            inferSchema=True
+                            )
     logging.info("Data Frame : 'sales_df' is built")
-    
+
     api = "sales"
     logging.info("Reading the CSV File into dataframe...")
 
     # Do the Transformation for the Sales Dataframe
     sales_df = sales_df \
-                .withColumnRenamed(sales_df.columns[0], "SALE_ID") \
-                .withColumnRenamed(sales_df.columns[1], "CUSTOMER_ID") \
-                .withColumnRenamed(sales_df.columns[2], "PRODUCT_ID") \
-                .withColumnRenamed(sales_df.columns[3], "SALE_DATE") \
-                .withColumnRenamed(sales_df.columns[4], "QUANTITY") \
-                .withColumnRenamed(sales_df.columns[5], "DISCOUNT") \
-                .withColumnRenamed(sales_df.columns[6], "SHIPPING_COST") \
-                .withColumnRenamed(sales_df.columns[7], "ORDER_STATUS") \
-                .withColumnRenamed(sales_df.columns[8], "PAYMENT_MODE")
+        .withColumnRenamed(sales_df.columns[0], "SALE_ID") \
+        .withColumnRenamed(sales_df.columns[1], "CUSTOMER_ID") \
+        .withColumnRenamed(sales_df.columns[2], "PRODUCT_ID") \
+        .withColumnRenamed(sales_df.columns[3], "SALE_DATE") \
+        .withColumnRenamed(sales_df.columns[4], "QUANTITY") \
+        .withColumnRenamed(sales_df.columns[5], "DISCOUNT") \
+        .withColumnRenamed(sales_df.columns[6], "SHIPPING_COST") \
+        .withColumnRenamed(sales_df.columns[7], "ORDER_STATUS") \
+        .withColumnRenamed(sales_df.columns[8], "PAYMENT_MODE")
     logging.info("Data Frame : Transformed 'sales_df' is built")
 
     # Populating the legacy version of Sales Data
     sales_df_lgcy = sales_df \
-                            .withColumn("DAY_DT", current_date()) \
-                            .select(
-                                col("DAY_DT"),
-                                col("SALE_ID"),
-                                col("CUSTOMER_ID"),
-                                col("PRODUCT_ID"),
-                                col("SALE_DATE"),
-                                col("QUANTITY"),
-                                col("DISCOUNT"),
-                                col("SHIPPING_COST"),
-                                col("ORDER_STATUS"),
-                                col("PAYMENT_MODE")
-                            )
+        .withColumn("DAY_DT", current_date()) \
+        .select(
+            col("DAY_DT"),
+            col("SALE_ID"),
+            col("CUSTOMER_ID"),
+            col("PRODUCT_ID"),
+            col("SALE_DATE"),
+            col("QUANTITY"),
+            col("DISCOUNT"),
+            col("SHIPPING_COST"),
+            col("ORDER_STATUS"),
+            col("PAYMENT_MODE")
+        )
     logging.info("Data Frame : 'sales_df_lgcy' is built")
 
-    try :
+    try:
 
         # Implement the Duplicate checker
         chk = DuplicateChecker()
@@ -302,7 +311,7 @@ def sales_data_ingestion():
         logging.error(str(e))
         raise
 
-    finally :
+    finally:
 
         # Abort the session when Done.
         abort_session(spark)
