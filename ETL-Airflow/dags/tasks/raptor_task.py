91d3c649e5b565fe54cf6d0f51578566a9c9f7e5
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2025-07-10 15:52:24 +0530

    Styled code in raptor_task

--- ETL-Airflow/dags/tasks/raptor_task.py
+++ ETL-Airflow/dags/tasks/raptor_task.py
@@ -3,29 +3,32 @@ from pyspark.sql.functions import *
 from pyspark.sql.window import *
 from pyspark.sql.types import *
 from tasks.my_secrets import USERNAME, PASSWORD
-from tasks.Raptor_MOD.Raptor import Raptor
+from Raptor.Raptor import Raptor
 from tasks.utils import get_spark_session
+
 
 @task
 def trigger_raptor():
 
     spark = get_spark_session()
-    
-    raptor_obj = Raptor(spark=spark,
-                    username=USERNAME,
-                    password=PASSWORD,)
-    
+
+    raptor_obj = Raptor(
+        spark=spark,
+        username=USERNAME,
+        password=PASSWORD,
+    )
+
     raptor_obj.submit_raptor_request(
         source_type='pg_admin',
         source_db='meta_morph',
         target_type='reporting',
         source_sql="""
-                    SELECT "DAY_DT", "SUPPLIER_ID", "SUPPLIER_NAME", "TOTAL_REVENUE", "TOTAL_PRODUCTS_SOLD", "TOTAL_STOCK_SOLD" FROM dev_legacy.supplier_performance
+                    SELECT * FROM legacy.supplier_performance
                     """,
         target_sql="""
-                    SELECT "DAY_DT", "SUPPLIER_ID", "SUPPLIER_NAME", "TOTAL_REVENUE", "TOTAL_PRODUCTS_SOLD", "TOTAL_STOCK_SOLD" FROM reporting.supplier_performance
+                    SELECT * FROM reporting.supplier_performance
                     """,
-        email='asritha.vig2338@gmail.com',
+        email='yateed1437@gmail.com',
         output_table_name='supplier_performance',
         primary_key='SUPPLIER_ID,DAY_DT'
     )
