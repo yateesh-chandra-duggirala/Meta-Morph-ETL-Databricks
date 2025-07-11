c77017cc64f3500076b9c19cfce1d98b55df106f
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2025-07-11 13:02:52 +0530

    Re-writing spark at task level

--- ETL-Airflow/dags/adhoc_job.py
+++ ETL-Airflow/dags/adhoc_job.py
@@ -5,7 +5,6 @@ from tasks.adhoc.adhoc_MM_145_20250711 import (
     load_prod_supplier_performance,
     push_data_to_reporting
 )
-from tasks.utils import get_spark_session, abort_session
 from tasks.m_customer_sales_report_task import customer_sales_report_ingestion
 
 
@@ -18,22 +17,11 @@ from tasks.m_customer_sales_report_task import customer_sales_report_ingestion
 )
 def ingestion():
 
-    try:
-        spark = get_spark_session()
+    prod_supplier_performance_task = load_prod_supplier_performance()
+    send_to_reporting_task = push_data_to_reporting()
+    csr_task = customer_sales_report_ingestion()
 
-        prod_supplier_performance_task = load_prod_supplier_performance(spark)
-        send_to_reporting_task = push_data_to_reporting(spark)
-        csr_task = customer_sales_report_ingestion()
-
-        prod_supplier_performance_task >> [send_to_reporting_task, csr_task]
-
-    except Exception as e:
-        raise e
-
-    finally:
-        abort_session(spark)
-
-    return "Adhoc job completed successfully.."
+    prod_supplier_performance_task >> [send_to_reporting_task, csr_task]
 
 
 # Call the Ingestion Dag Function
--- ETL-Airflow/dags/tasks/adhoc/adhoc_MM_145_20250711.py
+++ ETL-Airflow/dags/tasks/adhoc/adhoc_MM_145_20250711.py
@@ -3,18 +3,21 @@ import logging
 from tasks.utils import (
     write_into_table,
     read_data,
-    write_to_gcs
+    write_to_gcs,
+    abort_session,
+    get_spark_session
 )
 from pyspark.sql.functions import col, current_date
 
 
 # push_table_to_gcs_file
 @task(task_id="m_load_prod_supplier_performance")
-def load_prod_supplier_performance(spark):
+def load_prod_supplier_performance():
     """
     Consumes the table and writes into file.
     """
     try:
+        spark = get_spark_session()
         supplier_df = read_data(spark, "dev_legacy.supplier_performance")
         logging.info(supplier_df.count())
         write_into_table(
@@ -26,12 +29,15 @@ def load_prod_supplier_performance(spark):
     except Exception as e:
         logging.error(e)
         raise e
+    finally:
+        abort_session(spark)
     return "Loaded data from dev to Prod..!"
 
 
 @task(task_id="m_load_legacy_tables_to_gcs")
-def push_data_to_reporting(spark):
+def push_data_to_reporting():
     try:
+        spark = get_spark_session()
         table_name = 'supplier_performance'
         df = read_data(spark, f"legacy.{table_name}")
         today_df = df.filter(col("DAY_DT") == current_date())
@@ -40,4 +46,6 @@ def push_data_to_reporting(spark):
     except Exception as e:
         logging.error(e)
         raise
+    finally:
+        abort_session(spark)
     return f"{table_name} pushed to reporting"
